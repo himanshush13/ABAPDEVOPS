4fbbd1c9f7ebc534ecf26ca439309000472439f3
himanshush13 <36809136+himanshush13@users.noreply.github.com>
2020-01-20 12:18:10 +0530

    Update PipelineScript.groovy

--- PipelineScript.groovy
+++ PipelineScript.groovy
@@ -3,43 +3,35 @@ pipeline {
    stages{
          stage('ABAP Unit Test'){
                steps{
-                     withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'NID', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+                     withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: '', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
                      sh '''
-                     docker run -t postman/newman run https://github.com/mygit999/ABAPDevOps.git/ABAPDEVOPS/master/01_ABAP_Unit/abap_unit.postman_collection.json -k --bail --environment https://github.com/mygit999/ABAPDevOps.git/ABAPDEVOPS/master/01_ABAP_Unit/abap_unit.postman_environment.json -k --timeout-request 120000 --global-var "username=$USERNAME" --global-var "password=$PASSWORD" --global-var "package=zdevops" 
+                     newman run https://github.com/mygit999/ABAPDevOps.git/ABAPDEVOPS/master/01_ABAP_Unit/abap_unit.postman_collection.json -k --bail --environment https://github.com/mygit999/ABAPDevOps.git/ABAPDEVOPS/master/01_ABAP_Unit/abap_unit.postman_environment.json -k --timeout-request 120000 --global-var "username=$USERNAME" --global-var "password=$PASSWORD" --global-var "package=zdevops" 
                      '''
                      }
                 }
          }
         stage('ABAP Test Coverage'){
                steps{
-                    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'NID', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+                    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: '', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
                     sh ''' 
-                    docker run -t postman/newman run https://github.com/mygit999/ABAPDevOps.git/ABAPDEVOPS/master/02_ABAP_Coverage/abap_coverage_analysis.postman_collection.json -k --bail --environment https://github.com/mygit999/ABAPDevOps.git/ABAP_CI_PIPELINE_BASE/master/02_ABAP_Coverage/abap_coverage.postman_environment.json --timeout-request 120000 --global-var "username=$USERNAME" --global-var "password=$PASSWORD" --global-var "package=zdevops" 
+                    newman run https://github.com/mygit999/ABAPDevOps.git/ABAPDEVOPS/master/02_ABAP_Coverage/abap_coverage_analysis.postman_collection.json -k --bail --environment https://github.com/mygit999/ABAPDevOps.git/ABAP_CI_PIPELINE_BASE/master/02_ABAP_Coverage/abap_coverage.postman_environment.json --timeout-request 120000 --global-var "username=$USERNAME" --global-var "password=$PASSWORD" --global-var "package=zdevops" 
                     '''
                     }
                }
          }
         stage('ATC Checks'){
-              parallel{
-                    stage('ATC Checks HANA Readiness - FUNCTIONAL_DB'){
+             
+                    stage('ATC Checks DEFAULT'){
                         steps{
-                            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'NID', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+                            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: '', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
                             sh ''' 
-                           docker run -t postman/newman run https://github.com/mygit999/ABAPDevOps.git/ABAPDEVOPS/master/03_ATC_Checks/abap_atc.postman_collection.json -k --bail --environment https://github.com/mygit999/ABAPDevOps.git/ABAPDEVOPS/master/03_ATC_Checks/abap_atc_funcdb.postman_environment.json --timeout-request 120000 --global-var "username=$USERNAME" --global-var "password=$PASSWORD" --global-var "package=ZDEVOPS"
+                           newman run https://github.com/mygit999/ABAPDevOps.git/ABAPDEVOPS/master/03_ATC_Checks/abap_atc.postman_collection.json -k --bail --environment https://github.com/mygit999/ABAPDevOps.git/ABAPDEVOPS/master/03_ATC_Checks/abap_atc_funcdb.postman_environment.json --timeout-request 120000 --global-var "username=$USERNAME" --global-var "password=$PASSWORD" --global-var "package=ZDEVOPS"
                            '''
                            }
                         }
                      }
-                     stage('ATC Checks HANA Readiness - FUNCTIONAL_DB_ADDITION'){
-                           steps{
-                           withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'NID', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-                            sh ''' 
-                           docker run -t postman/newman run https://github.com/mygit999/ABAPDevOps.git/ABAPDEVOPS/master/03_ATC_Checks/abap_atc.postman_collection.json -k --bail --environment https://github.com/mygit999/ABAPDevOps.git/ABAPDEVOPS/master/03_ATC_Checks/abap_atc_funcdbadd.postman_environment.json --timeout-request 120000 --global-var "username=$USERNAME" --global-var "password=$PASSWORD" --global-var "package=ZDEVOPS"
-                           '''
-                           }
-                        }
-                     }
+                     
                }
-         }      
+               
    }
 }
